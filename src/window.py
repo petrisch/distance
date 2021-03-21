441df74f98dabbb1b37df54531adcc5d2b771f3b
petrisch <petrisch@virtubuntu2010.petrisch.duckdns.org>
2021-03-21 17:03:14 +0100

    little defactoring

--- src/window.py
+++ src/window.py
@@ -15,37 +15,29 @@ rangefinder = importlib.import_module("BOSCH-GLM-rangefinder.glm100c")
 #class Application(Gtk.Application):
 
 #    def __init__(self):
- #       super().__init__(application_id='com.petrisch.distance')
+#        super().__init__(application_id='com.petrisch.distance')
 #        GLib.set_application_name(_('Distance'))
- #       GLib.set_prgname('com.petrisch.distance')
+#        GLib.set_prgname('com.petrisch.distance')
 
- #   def do_activate(self):
-#        print("dont bother")
+#    def do_activate(self):
 #        builder = Gtk.Builder()
-        #When flatpak
-        #builder.add_from_resource('/com/petrisch/distance/src/distance_window.ui')
+#        When using flatpak
+#        builder.add_from_resource('/com/petrisch/distance/src/distance_window.ui')
 #        window = builder.get_object("DistanceWindow")
 #        window = Window(application=self)
 
-        #window.set_default_size(320, 512)
+#         window.set_default_size(320, 512)
 #        window.show_all()
 
 #        self.distance = Distance()
 #        self.distance.connect('changed', window.update_info)
 #        self.distance.claim()
 
-        #win = Gtk.Window()
-        #win.connect('delete-event', Gtk.main_quit)
 
-        #window = DistanceWindow()
-        #win.add(window)
-        #win.show_all()
-        #Gtk.main()
-
- #   def do_shutdown(self):
- #       #TODO: Shut down the connection when finished
-        #self.distance.release()
- #       return ""
+#    def do_shutdown(self):
+#        TODO: Shut down the connection when finished
+#         self.distance.release()
+#        return ""
 
 
 @GtkTemplate(ui='src/distance_window.ui')
@@ -86,28 +78,4 @@ def measure_distance(self):
 def debugmessage():
     print("Window module is loaded")
 
-
-
-#From David Boddie
-#    def update_info(self, sensor, near):
-#        if near:
-#            self.label.set_markup(
-#                _('<span size="large"><b>Near</b></span>'))
-#        else:
-#            self.label.set_markup(
-#                _('<span size="large">Far</span>'))
-
-#Thats how to connect the button
-#         self.box = Gtk.Box(spacing=6)
-#         self.add(self.box)
-#         self.button = Gtk.Button(label="Click Here")
-#         self.button.set_halign(Gtk.Align.CENTER)
-#         self.button.set_valign(Gtk.Align.CENTER)
-#         self.button.connect("clicked", self.on_button_clicked)
-#         self.box.pack_start(self.button, True, True, 0)
-
-#def measure_button_clicked_cb(self):
-#    distance = measure_distance()
-#    self.distance1_label.set_markup(
-#        _('<span size="large">Distance 1 is:' + distance + '</span>'))
 
