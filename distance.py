30db0a020f6df79c823d8eb987044a2f20d93228
Patrick Jörg <patrickjoerg@gmx.ch>
2021-11-08 21:13:32 +0100

    new approach: label ansprechen

--- distance.py
+++ distance.py
@@ -31,7 +31,7 @@ def measure_distance(self):
         if not glm50c.connected:
             distance = "No Connection"
             self.set_label(distance)
-        distance = glm50c.measure_from_tripod_socket(glm50c)
+        distance = glm50c.measure_from_tripod_socket()
         if distance != -1:
             self.set_label(distance)
         else:
